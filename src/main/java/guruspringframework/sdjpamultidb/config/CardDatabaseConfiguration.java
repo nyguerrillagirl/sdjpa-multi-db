834b8197627375b83ef595f28c1eb343f3e5c693
nyguerrillagirl <lorraine.figueroa@brainycode.com>
2023-12-10 08:23:00 -0500

    Completed Lecture 209. Data Source Properties

---
+++ src/main/java/guruspringframework/sdjpamultidb/config/CardDatabaseConfiguration.java
@@ -0,0 +1,15 @@
+package guruspringframework.sdjpamultidb.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CardDatabaseConfiguration {
+    @Bean
+    @ConfigurationProperties("spring.card.datasource")
+    public DataSourceProperties cardDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+}
