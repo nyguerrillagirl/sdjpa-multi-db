425e42e88f35f4087021c19621e9594621c8f124
nyguerrillagirl <lorraine.figueroa@brainycode.com>
2023-12-10 16:24:26 -0500

    Completed Lecture 223. Save New Credit Card.

--- src/test/java/guruspringframework/sdjpamultidb/SdjpaMultiDbApplicationTests.java
+++ src/test/java/guruspringframework/sdjpamultidb/SdjpaMultiDbApplicationTests.java
@@ -1,10 +1,37 @@
 package guruspringframework.sdjpamultidb;
 
+import guruspringframework.sdjpamultidb.domain.creditcard.CreditCard;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import guruspringframework.sdjpamultidb.services.CreditCardService;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class SdjpaMultiDbApplicationTests {
+
+    @Autowired
+    CreditCardService creditCardService;
+
+    @Test
+    void testSaveAndGetCreditCard() {
+        CreditCard cc = CreditCard.builder()
+                .firstName("Lorraine")
+                .lastName("Figueroa")
+                .zipCode("11208")
+                .creditCardNumber("1234567890000")
+                .cvv("123")
+                .expirationDate("12/26")
+                .build();
+
+        CreditCard savedCC = creditCardService.saveCreditCard(cc);
+
+        assertThat(savedCC).isNotNull();
+        assertThat(savedCC.getId()).isNotNull();
+        assertThat(savedCC.getCreditCardNumber()).isNotNull();
+    }
 
     @Test
     void contextLoads() {
